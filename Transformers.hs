11abf4c553b08d619c0c63542c33e0b2aeea0cc3
Derek McLoughlin <derek.mcloughlin@gmail.com>
2014-04-21 17:43:24 +0100

    Swap State and ErrorT - not working

--- Transformers.hs
+++ Transformers.hs
@@ -234,4 +234,44 @@ eval4 (App e1 e2)   = do
                             _ -> throwError "type error in App"
 
 
+-- Trying it with StateT and ErrorT swapped
+
+type Eval4' a = ReaderT Env (StateT Integer (ErrorT String Identity)) a
+
+runEval4' :: Env -> Integer -> Eval4' a -> (Either String (a, Integer))
+runEval4' env st ev = runIdentity (runErrorT (runStateT (runReaderT ev env)) st)
+
+eval4' :: Exp -> Eval4' Value
+eval4' (Lit i)       = do
+                        tick
+                        return $ IntVal i
+eval4' (Var n)       = do
+                        tick
+                        env <- ask
+                        case Map.lookup n env of
+                            Just val -> return val
+                            Nothing  -> throwError ("unbound variable " ++ n)
+eval4' (Plus e1 e2)  = do
+                        tick
+                        e1' <- eval4' e1
+                        e2' <- eval4' e2
+                        case (e1', e2') of 
+                            (IntVal i1, IntVal i2) -> 
+                                return $ IntVal (i1 + i2)
+                            _ ->
+                                throwError "type error in Plus"
+eval4' (Abs n e)     = do
+                        tick
+                        env <- ask
+                        return $ FunVal env n e
+eval4' (App e1 e2)   = do
+                        tick
+                        env <- ask
+                        val1 <- eval4' e1
+                        val2 <- eval4' e2
+                        case val1 of
+                            FunVal env' n body ->
+                                local (const (Map.insert n val2 env')) (eval4' body)
+                            _ -> throwError "type error in App"
+
 
